4b3863e0d5d1586d7ac2436bb100f8241ba1b422
shtsukada <st01251988@gmail.com>
2020-08-07 09:07:31 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
 
   def create
@@ -13,6 +14,7 @@ class MessagesController < ApplicationController
       render :index
     end
   end
+
 
   private
 
